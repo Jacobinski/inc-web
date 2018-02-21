d1f9200810b9d8a37aabe6b4485329ac1f2ebabe
Rahat Dhande <rahatchd@gmail.com>
2018-02-20 20:39:04 -0800

    adds barebones express server

---
+++ src/server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import React, {Component} from 'react';
+
+const app = express();
+const PORT = 8080;
+
+app.get('/', (request, response) => {
+    response.send(<h1>Hi this is dog</h1>);
+});
+
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+});
