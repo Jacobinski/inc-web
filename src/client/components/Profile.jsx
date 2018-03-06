7ffeac5156adda6c76cc5971bf68bc640a4f368c
Rahat Dhande <rahatchd@gmail.com>
2018-03-06 13:31:26 -0800

    adds datepicker

--- src/client/components/Profile.jsx
+++ src/client/components/Profile.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React from "react";
+import Calendar from "react-calendar";
 const {Component} = React;
 
 export default class Profile extends Component {
     constructor(props) {
         super(props);
-        this.state = {content: 'Loading'};
+        this.state = {date: new Date(), content: 'Loading'};
+
+        this._selectDate = this._selectDate.bind(this);
     }
 
     componentWillMount() {
@@ -19,11 +22,17 @@ export default class Profile extends Component {
             })
     }
 
+    _selectDate(date) {
+        this.setState({date});
+        console.log(this.state);
+    }
+
     render() {
         return (
             <div>
                 <h2>Profile</h2>
                 <p>{this.state.content}</p>
+                <Calendar onChange={this._selectDate} value={this.state.date}/>
             </div>
         );
     }
