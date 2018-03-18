3655a20a4f04f05401dd0d273e11755f034830cd
Rahat Dhande <rahatchd@gmail.com>
2018-03-17 19:44:54 -0700

    configures data to display name properly and use ec2

--- src/client/constants.js
+++ src/client/constants.js
@@ -1,17 +1,2 @@
-export const EXERCISE_TYPES =[
-    'Bench',
-    'Squat',
-    'Deadlift',
-    'Overhead Press'
-];
-
-export const EXERCISE_IMG = [
-    'https://img.aws.livestrongcdn.com/ls-article-image-673/ds-photo/getty/article/181/216/590141448_XS.jpg',
-    'https://cdn-maf0.heartyhosting.com/sites/muscleandfitness.com/files/styles/full_node_image_1090x614/public/media/barbell-back-squat_0.jpg?itok=H0ZAwp83',
-    'http://s3.amazonaws.com/hawtpress/wp-content/uploads/2016/10/17150530/Screen-Shot-2016-10-17-at-12.54.50-PM.png',
-    'https://www.bodybuilding.com/fun/images/2015/how-to-overhead-press-a-beginners-guide-graphics-1.jpg'
-];
-
 export const SEC_TO_MSEC = 1000;
-
 export const PING_INTERVAL_MSEC = 20000;
